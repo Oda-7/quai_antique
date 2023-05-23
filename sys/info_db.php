6d27bc55a35ba1d12d1f6fdd8562db4b0f5bc0eb
Oda-7 <104594516+Oda-7@users.noreply.github.com>
2023-05-23 19:30:37 +0200

    Update info_db.php

--- sys/info_db.php
+++ sys/info_db.php
@@ -5,9 +5,4 @@ if ($_SERVER['SERVER_NAME'] == "localhost") {
    $database = 'quaiantique';
    $user_name = 'admin';
    $password = 'quaiantique';
-} else {
-   $host_name = 'db5013125203.hosting-data.io';
-   $database = 'dbs11016128';
-   $user_name = 'dbu1243084';
-   $password = 'quai.AntiqueArnaud3';
-}
+} 
